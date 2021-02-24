668895d867d19a5a7011b23e5ed5379c790f7089
Pratik Gautam <prgaut@microsoft.com>
2021-02-24 14:37:51 +0530

    updating

--- GuidedExperience/BootstrapTour/app/app.js
+++ GuidedExperience/BootstrapTour/app/app.js
@@ -2,20 +2,27 @@
 var tour = new Tour({
     debug: true,
     orphan: false,
-    backdrop: true,
-    backdropContainer: 'div',
-    smartPlacement: true,
     storage: window.sessionStorage,
     steps: [
         {
             element: "#step1",
             title: "Step 1",
+            placement:"bottom",
             content: "This is the header of this webpage that can help us to understand more details about it"
+           
         },
         {
             element: "#step2",
             title: "Step 2",
+            placement: "bottom",
             content: "This is the middle section that can review the various libraries needed for this operation"
+        },
+        {
+            element: "#cars",
+            title: "Steo 3",
+            placement: "top",
+            content: "This is for testing a dropdown highlighting",
+            backdrop: true
         }
     ]
 });
