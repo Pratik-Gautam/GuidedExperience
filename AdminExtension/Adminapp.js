ebc12914f8a325ffc1c51b6de73096c1188ba4c3
Pratik-Gautam <pratik.gautam@outlook.com>
2021-05-31 10:09:35 +0530

    Add files via upload

---
+++ AdminExtension/Adminapp.js
@@ -0,0 +1,43 @@
+﻿var template="";
+template += "<div id=\"theroom-info\">";
+template += "  <span id=\"theroom-tag\"><\/span>";
+template += "  <span id=\"theroom-id\"><\/span>";
+template += "  <span id=\"theroom-class\"><\/span>";
+template += "<\/div>";
+template += "<button type=\"button\">Generate File</button>";
+template += "";
+template += "<style>";
+template += "  #theroom-info {";
+template += "    position: fixed;";
+template += "    bottom: 0;";
+template += "    width: 100%;";
+template += "    left: 0;";
+template += "    font-family: \"Courier\";";
+template += "    background-color: #333;";
+template += "    padding: 10px;";
+template += "    color: #fafafa;";
+template += "    text-align: center;";
+template += "    box-shadow: 0px 4px 20px rgba(0,0,0,0.3);";
+template += "  }";
+template += "";
+template += "  #theroom-tag {";
+template += "    color: #C2185B;";
+template += "  }";
+template += "";
+template += "  #theroom-id {";
+template += "    color: #5D4037;";
+template += "  }";
+template += "";
+template += "  #theroom-class {";
+template += "    color: #607D8B;";
+template += "  }";
+template += "<\/style>";
+
+var options = {
+  template: template,
+  showInfo: true
+};
+
+// initialize
+theRoom.start(options);
+
