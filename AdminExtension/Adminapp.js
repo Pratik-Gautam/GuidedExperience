171c6cdae41f479b56080e5815636ba79f86f828
Pratik-Gautam <pratik.gautam@outlook.com>
2021-06-01 19:58:47 +0530

    Adding code change to get the item working

--- AdminExtension/Adminapp.js
+++ AdminExtension/Adminapp.js
@@ -1,8 +1,8 @@
 var template="";
 template += "<div id=\"theroom-info\">";
 template += "  <span id=\"highlight\">Please hover on the element that you want to add to the guided experience and click to add it!<\/span><br>";
-template += "  <button id=\"btnGenerateFile\" type=\"button\" onclick=\"alert(selectedSteps);\">Generate File</button>";
-template += "  <button id=\"btnClearSelection\" type=\"button\" onclick=\"alert(selectedSteps);\">Clear Selection</button>";
+template += "  <button class=\"exclusion\" id=\"btnGenerateFile\" type=\"button\" onclick=\"alert('Hi');\">Generate File</button>";
+template += "  <button class=\"exclusion\" id=\"btnClearSelection\" type=\"button\" onclick=\"alert('Hi');\">Clear Selection</button>";
 template += "<\/div>";
 template += "";
 template += "<style>";
@@ -16,7 +16,6 @@ template += "    background-color: #333;";
 template += "    padding: 10px;";
 template += "    color: #fafafa;";
 template += "    text-align: center;";
-template += "    z-index: -1;";
 template += "    box-shadow: 0px 4px 20px rgba(0,0,0,0.3);";
 template += "  }";
 template += "";
@@ -25,13 +24,12 @@ template += "    color: #FFFF00;";
 template += "  }";
 template += "  #btnGenerateFile {";
 template += "    background-color: #4CAF50;";
-template += "    z-index: 2147483647;";
+template += "    pointer-events: auto;";
 template += "  }";
 template += "  #btnClearSelection {";
 template += "    background-color: #4CAF50;";
-template += "    z-index: 2147483647;";
+template += "    pointer-events: auto;";
 template += "  }";
-
 template += "<\/style>";
 
 var options = {
