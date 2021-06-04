ea5001ad9a849020bcb1873966bde029bc82d3a6
Pratik-Gautam <pratik.gautam@outlook.com>
2021-06-04 17:35:28 +0530

    removed specifics
    removed specifics

--- AdminExtension/AdminExtension.js
+++ AdminExtension/AdminExtension.js
@@ -1,17 +1,17 @@
 
 
 var theRoom_reference = document.createElement('script');
-theRoom_reference.setAttribute('src','https://prgautteststorage.blob.core.windows.net/guidedexperienceresources/dompath.js');
+theRoom_reference.setAttribute('src','<Blob storage url>/guidedexperienceresources/dompath.js');
 theRoom_reference.setAttribute('type','text/javascript');
 (document.head||document.documentElement).appendChild(theRoom_reference);
 
 
 var theRoom_reference = document.createElement('script');
-theRoom_reference.setAttribute('src','https://prgautteststorage.blob.core.windows.net/guidedexperienceresources/TheRoom-Final.js');
+theRoom_reference.setAttribute('src','<Blob storage url>/guidedexperienceresources/TheRoom-Final.js');
 theRoom_reference.setAttribute('type','text/javascript');
 (document.head||document.documentElement).appendChild(theRoom_reference);
 
 var adminApp_reference = document.createElement('script');
-adminApp_reference.setAttribute('src','https://prgautteststorage.blob.core.windows.net/guidedexperienceresources/Adminapp.js');
+adminApp_reference.setAttribute('src','<Blob storage url>/guidedexperienceresources/Adminapp.js');
 adminApp_reference.setAttribute('type','text/javascript');
 (document.head||document.documentElement).appendChild(adminApp_reference);
